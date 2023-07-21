a1489096534bfc136ce2ba37a64ec9bef0658a74
patrictobola <tobola92@gmail.com>
2023-07-21 16:10:20 +0200

    moved movie class in models folder

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
 <?php
-class Movie
-{
 
-    public $title;
-    public $actors;
-    public $genre;
-
-    // Constructor 
-    public function __construct($title, $actors, $genre)
-    {
-        $this->title = $title;
-        $this->actors = $actors;
-        $this->genre = $genre;
-    }
-
-    // method 
-    // public function setTitle($text)
-    // {
-    //     return ucfirst($text);
-    // }
-
-    public function getMovieInfo()
-    {
-        return "Il nome del film è: $this->title, <br> Il protagonista è: $this->actors, <br> Il genere del film è: $this->genre";
-    }
-}
-
+require __DIR__ . '/models/Movie.php';
 
 $film_1 = new Movie('Borat', 'Sasha Baron Cohen', 'Comic');
 $film_2 = new Movie('Oppenheimer', 'Cillia Murphy', 'Thriller');
---
+++ models/Movie.php
@@ -0,0 +1,27 @@
+<?php
+class Movie
+{
+
+    public $title;
+    public $actors;
+    public $genre;
+
+    // Constructor 
+    public function __construct($title, $actors, $genre)
+    {
+        $this->title = $title;
+        $this->actors = $actors;
+        $this->genre = $genre;
+    }
+
+    // method 
+    // public function setTitle($text)
+    // {
+    //     return ucfirst($text);
+    // }
+
+    public function getMovieInfo()
+    {
+        return "Il nome del film è: $this->title, <br> Il protagonista è: $this->actors, <br> Il genere del film è: $this->genre";
+    }
+}
