99a703a2f9e88275eb43f4b575e415fd7ae9eedc
patrictobola <tobola92@gmail.com>
2023-07-21 16:13:35 +0200

    ordered filex and linked each others with require

---
+++ data/movies.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../models/Movie.php';
+
+
+$film_1 = new Movie('Borat', 'Sasha Baron Cohen', 'Comic');
+$film_2 = new Movie('Oppenheimer', 'Cillia Murphy', 'Thriller');
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 require __DIR__ . '/models/Movie.php';
+include __DIR__ . '/data/movies.php';
 
-$film_1 = new Movie('Borat', 'Sasha Baron Cohen', 'Comic');
-$film_2 = new Movie('Oppenheimer', 'Cillia Murphy', 'Thriller');
 var_dump($film_1, $film_2);
 
 echo $film_1->getMovieInfo();
